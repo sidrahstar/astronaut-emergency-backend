c4ce794700feed8a2cabbe29ab4cb8bd696ac67a
sidrah <sidrahstardust@gmail.com>
2026-09-25 14:26:51 +0530

    Updated backend

--- main.py
+++ main.py
@@ -6,25 +6,31 @@ app = FastAPI()
 @app.get("/emergency/{emergency_id}")
 def get_emergency(emergency_id: str):
 
-    conn = sqlite3.connect("emergencies.db")
-    cursor = conn.cursor()
+    try:
+        conn = sqlite3.connect("emergencies.db")
+        cursor = conn.cursor()
 
-    cursor.execute(
-        "SELECT * FROM emergencies WHERE emergency_id = ?",
-        (emergency_id,)
-    )
+        cursor.execute(
+            "SELECT * FROM emergencies WHERE emergency_id = ?",
+            (emergency_id,)
+        )
 
-    row = cursor.fetchone()
-    conn.close()
+        row = cursor.fetchone()
+        conn.close()
 
-    if row is None:
-        return {"error": "Emergency not found"}
+        if row is None:
+            return {"error": "Emergency not found"}
 
-    return {
-        "emergency_id": row[0],
-        "name": row[1],
-        "priority": row[2],
-        "keywords": row[3],
-        "procedure": row[4],
-        "checklist": row[5]
-    }
+        return {
+            "emergency_id": row[0],
+            "name": row[1],
+            "priority": row[2],
+            "keywords": row[3],
+            "procedure": row[4],
+            "checklist": row[5]
+        }
+
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
